f80918a67b618c0f84d131ff331457c573119d97
smr <mohammad>
2022-08-27 12:25:29 +0430

    Part 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.implicitly_wait(10)
@@ -9,5 +11,14 @@ driver.get('http://localhost:5000/examples/1')
 my_el = driver.find_element(By.ID,'btn')
 my_el.click()
 
-com_el = driver.find_element(By.ID,'title')
-print(com_el.text) # dosen't show complete message so we need to use explicit wait
+my_el = driver.find_element(By.ID,'title')
+print(my_el.text)
+
+WebDriverWait(driver,100).until(
+    EC.text_to_be_present_in_element(
+        (By.ID,'title'),
+        'Completed'
+    )
+)
+
+print(my_el.text)
